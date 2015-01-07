68a5265627b46a001e80403607068638de9ecfb3
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-01-07 01:39:14 -0700

    finished simple string, very fun

---
+++ string_basics.rb
@@ -0,0 +1,42 @@
+require 'pry'
+class SimpleString
+  attr_reader :data
+
+  def initialize(the_data)
+    @data = the_data
+  end
+
+  def length_new
+    @data.chars.count { |letter| letter }
+  end
+
+  def end_with_new?(ends_with)
+  count = 0
+  block_end_string = ends_with.chars.reverse
+
+  block_end_string.each do |letter|
+    count += 1
+    if letter == @data.chars[-count]
+      true
+    else
+      return false
+    end
+  end
+  end
+
+  def upcase_new
+    lower_alpha = ("a".."z").to_a.join
+    upper_alpha = ("A".."Z").to_a.join
+    if @data == ''
+      return ""
+    else
+    return @data.tr(lower_alpha, upper_alpha)
+  end
+  end
+
+  def +(to_be_added)
+    sum = [@data.chars, to_be_added.data.chars].join
+    sum
+  end
+
+end
---
+++ string_basics_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'string_basics'
+
+class SimpleStringTest < Minitest::Test
+
+  def string
+    @string
+  end
+
+  def setup
+    @string = SimpleString.new('abc')
+  end
+
+  def test_it_can_implement_length
+    a_string = SimpleString.new("hello")
+    assert_equal 5, a_string.length_new
+    second_string = SimpleString.new("")
+    assert_equal 0, second_string.length_new
+  end
+
+  def test_it_can_implement_end_with
+    a_string = SimpleString.new("hello")
+    assert a_string.end_with_new?("o")
+    refute a_string.end_with_new?("x")
+    assert a_string.end_with_new?("ello")
+    refute a_string.end_with_new?("ell")
+    refute a_string.end_with_new?("yllo")
+  end
+
+  def test_it_can_implement_upcase
+    a_string = SimpleString.new("hello")
+    second_string = SimpleString.new("BYE")
+    third_string = SimpleString.new("")
+    assert_equal "HELLO", a_string.upcase_new
+    assert_equal "BYE", second_string.upcase_new
+    assert_equal "", third_string.upcase_new
+  end
+
+
+  def test_it_can_implement_plus
+    a_string = SimpleString.new("hello")
+    second_string = SimpleString.new("BYE")
+    assert "helloBYE", a_string + second_string
+  end
+
+end
